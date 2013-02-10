3c3b439550ed9bc436a0c9393d70d60e7984cbf7
richmaxim <richmaxim@localhost>
2013-02-10 22:26:07 +0100

    Forgotten files for VKontatkte

---
+++ ViewModels/VKontakteAccessTokenJsonModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace RM.QuickLogOn.OAuth.RU.ViewModels
+{
+    [DataContract]
+    public class VKontakteAccessTokenJsonModel
+    {
+        [DataMember]
+        public string access_token { get; set; }
+
+        [DataMember]
+        public string user_id { get; set; }
+
+        [DataMember]
+        public string expires_in { get; set; }
+    }
+}
---
+++ ViewModels/VKontakteUserInfoJsonViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace RM.QuickLogOn.OAuth.RU.ViewModels
+{
+    [DataContract]
+    public class VKontakteUserInfoJsonViewModel
+    {
+        public class UserInfo
+        {
+            [DataMember]
+            public string uid { get; set; }
+
+            [DataMember]
+            public string first_name { get; set; }
+            
+            [DataMember]
+            public string last_name { get; set; }
+        }
+
+        [DataMember]
+        public UserInfo[] response { get; set; }
+    }
+}
